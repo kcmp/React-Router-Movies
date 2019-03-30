8990db1894a5a72468d714fb38399f36ddee21fc
unknown <kcmp711@gmail.com>
2019-03-30 14:24:49 -0400

    added NavLink to SavedList.js

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -4,26 +4,29 @@ import { Link } from 'react-router-dom';
 function MovieCard({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <div className="save-wrapper">
+        <div className="movie-card">
 
-      <Link to={`Movies/${movie.id}`}>
-        <h2>{title}</h2>
-      </Link>
+        <Link to={`Movies/${movie.id}`}>
+          <h2>{title}</h2>
+        </Link>
 
-      <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
+          <div className="movie-director">	
+            Director: <em>{director}</em>	
+          </div>	
+          <div className="movie-metascore">	
+            Metascore: <strong>{metascore}</strong>	
+          </div>	
+          <h3>Actors</h3>	
 
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}      
-        <div className="save-button">Save</div>
+
+           {stars.map(star => (	
+            <div key={star} className="movie-star">	
+              {star}	
+            </div>	
+          ))}	
+        </div>	
+        <div className="save-button">Save</div>	
       </div>
   );
 }
